9e4a9d01c818c6d41a3c81357ec3abfc62a8e59d
Camilo Lopes <camilo@camilolopes.com.br>
2013-10-30 13:25:14 -0200

    added dialog example

--- receipeangularexample/src/main/webapp/js/app.js
+++ receipeangularexample/src/main/webapp/js/app.js
@@ -1,40 +1,119 @@
-var app = angular.module("myapp",["ui","ui.directives"]);
+var app = angular.module("myapp", [ "ui", "ui.directives","ui.bootstrap.modal" ]);
 
-//creating route
-app.config(function($routeProvider,$httpProvider,$locationProvider){
-	$routeProvider.
-	when("/",{templateUrl:"home.html"}).
-	when("/upper",{templateUrl:"upperfilter.html"}).
-	when("/serviceioc",{templateUrl:"serviceioc.html"}).
-	when("/checkme",{templateUrl:"checkme.html"}).
-	when("/locale",{templateUrl:"locale.html"}).
-	when("/angularui",{templateUrl:"angularui.html",controller:blacklistController}).
-	when("/filtername",{templateUrl:"filterorder.html",controller:filterController}).
-	when("/watch",{templateUrl:"watch.html",controller:recipeController});
-	
+
+// creating route
+app.config(function($routeProvider, $httpProvider, $locationProvider) {
+	$routeProvider.when("/", {
+		templateUrl : "home.html"
+	}).when("/upper", {
+		templateUrl : "upperfilter.html"
+	}).when("/serviceioc", {
+		templateUrl : "serviceioc.html"
+	}).when("/checkme", {
+		templateUrl : "checkme.html"
+	}).when("/locale", {
+		templateUrl : "locale.html"
+	}).when("/angularui", {
+		templateUrl : "angularui.html",
+		controller : blacklistController
+	}).when("/filtername", {
+		templateUrl : "filterorder.html",
+		controller : filterController
+	}).when("/paginating", {
+		templateUrl : "paginating.html",
+		controller : PaginationCtrl
+	}).when("/paginating2", {
+		templateUrl : "paginating2.html",
+		controller : PaginationCtrl2
+	}).
+	when("/editavel", {
+		templateUrl : "editavel.html"
+	}).
+	when("/dialog", {
+		templateUrl : "dialog.html",controller:MyCtrlDialog 
+	}).
+	when("/watch", {
+		templateUrl : "watch.html",
+		controller : recipeController
+	});
+
 });
-//creating my service
-app.factory("UserService",function(){
-	var users = ["Ivete", "Camilo", "Lopes"];
-	
-	return{
-		all:function(){
+// creating my service
+app.factory("UserService", function() {
+	var users = [ "Ivete", "Camilo", "Lopes" ];
+
+	return {
+		all : function() {
 			return users;
 		},
-		first:function(){
+		first : function() {
 			return users[0];
 		}
 	};
 });
 
 
-//creating directive name
-app.directive("show",function(){
-	return{
-		link:function(scope,element,attributes){
-			scope.$watch(attributes.show,function(value){
-				element.css('display',value ? '':'none');
+//creating Item service for pagination
+app.factory("Item", function() {
+	var items = [ {
+		name : "Kent Beck",
+		description : "developer"
+	}, {
+		name : "Camilo",
+		description : "Developer"
+	},
+	{
+		name : "Jobs",
+		description : "CEO"
+	},
+	];
+
+	for ( var i = 0; i < 50; i++) {
+		items.push({
+			id : i,
+			name : "name " + i,
+			description : "description " + i
+		});
+	}
+	return {
+		get : function(offset, limit) {
+			return items.slice(offset, offset + limit);
+		},
+		total : function() {
+			return items.length;
+		}
+	};
+});
+
+// creating directive name
+app.directive("show", function() {
+	return {
+		link : function(scope, element, attributes) {
+			scope.$watch(attributes.show, function(value) {
+				element.css('display', value ? '' : 'none');
 			});
 		}
 	};
+});
+
+//criando campo editavel
+app.directive("contenteditable",function(){
+	return {
+		restrict:"A",
+		require: "ngModel",
+		link:function(scope,element, attrs, ngModel){
+			//notifica o angular sobre a mudança
+			function read(){
+				ngModel.$setViewValue(element.html());
+			}
+			
+			ngModel.$render = function(){
+				element.html(ngModel.$viewValue|| "");
+			};
+			element.bind("blur keyup change", function(){
+				scope.$apply(read);
+			});
+		}
+	};
+	
 });
--- receipeangularexample/src/main/webapp/js/recipeController.js
+++ receipeangularexample/src/main/webapp/js/recipeController.js
@@ -17,9 +17,9 @@ var anotherController = function($scope, UserService) {
 anotherController.$inject = [ "$scope", "UserService" ];
 
 var blacklistController = function($scope) {
-	//criando meu black list, um array. 
+	// criando meu black list, um array.
 	$scope.blacklist = [ 'satanas', 'biba' ];
-	//verifica se a palavra digitada corresponde ao que temos no blacklist
+	// verifica se a palavra digitada corresponde ao que temos no blacklist
 	$scope.notBlackListed = function(value) {
 		return $scope.blacklist.indexOf(value) === -1;
 	};
@@ -39,4 +39,117 @@ var filterController = function($scope) {
 		name : "Ana",
 		age : 30
 	} ];
+};
+
+var PaginationCtrl = function($scope) {
+	$scope.itemsPerPage = 5;
+	$scope.currentPage = 0;
+	$scope.items = [];
+
+	for ( var i = 0; i < 50; i++) {
+		$scope.items.push({
+			id : i,
+			name : "name " + i,
+			description : "description " + i
+		});
+	}
+	$scope.range = function() {
+		var rangeSize = 5;
+		var ret = [];
+		var start;
+
+		start = $scope.currentPage;
+		if (start > $scope.pageCount() - rangeSize) {
+			start = $scope.pageCount() - rangeSize + 1;
+		}
+
+		for ( var i = start; i < start + rangeSize; i++) {
+			ret.push(i);
+		}
+		return ret;
+	};
+	$scope.prevPage = function() {
+		if ($scope.currentPage > 0) {
+			$scope.currentPage--;
+		}
+	};
+	$scope.prevPageDisabled = function() {
+		return $scope.currentPage === 0 ? "disabled" : "";
+	};
+	$scope.pageCount = function() {
+		return Math.ceil($scope.items.length / $scope.itemsPerPage) - 1;
+	};
+	$scope.nextPage = function() {
+		if ($scope.currentPage < $scope.pageCount()) {
+			$scope.currentPage++;
+		}
+	};
+	$scope.nextPageDisabled = function() {
+		return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+	};
+};// end of paginaiton controller
+
+var PaginationCtrl2 = function($scope, Item) {
+	$scope.itemsPerPage = 5;
+	$scope.currentPage = 0;
+
+	$scope.range = function() {
+		var rangeSize = 5;
+		var ret = [];
+		var start;
+
+		start = $scope.currentPage;
+		if (start > $scope.pageCount() - rangeSize) {
+			start = $scope.pageCount() - rangeSize + 1;
+		}
+
+		for ( var i = start; i < start + rangeSize; i++) {
+			ret.push(i);
+		}
+		return ret;
+	};
+	$scope.prevPage = function() {
+		if ($scope.currentPage > 0) {
+			$scope.currentPage--;
+		}
+	};
+	$scope.prevPageDisabled = function() {
+		return $scope.currentPage === 0 ? "disabled" : "";
+	};
+	$scope.pageCount = function() {
+		return Math.ceil($scope.total / $scope.itemsPerPage);
+	};
+	$scope.nextPage = function() {
+		if ($scope.currentPage < $scope.pageCount()) {
+			$scope.currentPage++;
+		}
+	};
+
+	$scope.setPage = function(n) {
+		if (n > 0 && n < $scope.pageCount()) {
+			$scope.currentPage = n;
+		}
+	};
+	$scope.nextPageDisabled = function() {
+		return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+	};
+	$scope.$watch("currentPage", function(newValue, oldValue) {
+		$scope.pagedItems = Item.get(newValue * $scope.itemsPerPage,
+				$scope.itemsPerPage);
+		$scope.total = Item.total;
+	});
+};
+
+var MyCtrlDialog = function($scope) {
+	$scope.open = function() {
+		$scope.showModal = true;
+	};
+
+	$scope.ok = function() {
+		$scope.showModal = false;
+	};
+
+	$scope.cancel = function() {
+		$scope.showModal = false;
+	};
 };
